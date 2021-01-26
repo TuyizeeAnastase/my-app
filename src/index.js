506d096322be147c220d5d4ea5690701eb81e55d
TuyizeeAnastase <tuyizereanastase1@gmail.com>
2021-01-26 15:29:34 +0200

    add components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-function getBtnText(){
- 
-    return 'Click Me';
-
-};
+import faker from 'faker';
 
 const App=()=>{
-
-
-    return <div>
-    <label className="label" for="name">Enter a name:</label>
-    <input id="name" type="text"/>
-    <button style={{backgroundColor:'blue',color:'white'}}>{getBtnText()}</button>
-  </div>
+    return (
+        <div className="ui container comments">
+        <div className="comment">
+        <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.image()}/>
+        </a>
+        <div className="content">
+        <a href="/" className="author">
+        Sam</a>
+        <div className="metadata">
+        <span className="date">Today at 6:00</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+        </div>
+        </div>
+        </div>
+    );
 }
 
 ReactDom.render(<App/>,document.querySelector('#root'));
